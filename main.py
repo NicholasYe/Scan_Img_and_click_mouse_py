a8d83ee48fa42258424d042624a05690137e104b
NicholasYe <2216393769@qq.com>
2022-04-30 02:47:52 +0800

    4.30_2:47

--- main.py
+++ main.py
@@ -3,26 +3,29 @@ import pyautogui
 from PIL import ImageGrab, Image
 import keyboard
 import os
+import goto
+from goto import with_goto
+from goto import goto, label
 
 # path setting
-path = 'E:\WPS_Sync_Files\py_mouse_click'
+path = 'E:/WPS_Sync_Files/py_mouse_click/IMG/'
 files = os.listdir(path)
-num_png = len(files)-2
+num_png = len(files)
 
 # Dataset of picture
 Screen_Img = []
-for i in range(1,num_png):
-    temp = Image.open(str(i)+'.jpeg')
+for i in range(1,num_png+1):
+    temp = Image.open(path+str(i)+'.jpeg')
     Screen_Img.append(temp)
 print('Successfully save, the numeber of picture is', len(Screen_Img))
 
-# run code and if you want to stop, just press 's' on keyboard
-while True:
-    if keyboard.is_pressed('s'):
-        break
-    for m in range(0,num_png-1):
+# core function and if you want to stop just press 's' on keyboard
+def func():
+    for m in range(0,num_png):
         Img_judge = pyautogui.locateOnScreen(Screen_Img[m],grayscale=True,confidence=.9)
-        if Img_judge == None:
+        if keyboard.is_pressed('s'):
+            exit()
+        elif Img_judge == None:
             print("can't find the same Img")
         else:
             x,y,width,height = Img_judge
@@ -31,3 +34,7 @@ while True:
             x+=10
             y+=10
             pyautogui.click(x,y,button='left')
+
+if __name__ == "__main__":
+    while(1):
+        func()
