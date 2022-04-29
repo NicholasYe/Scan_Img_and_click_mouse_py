06b8a68c54f273b04af563da3456fd95d9cc7991
NicholasYe <2216393769@qq.com>
2022-04-29 23:44:31 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from time import sleep
+import pyautogui
+from PIL import ImageGrab, Image
+import keyboard
+
+# Screen short in advance and save it as template
+Screen_Img= Image.open("screen.jpeg")
+msg = pyautogui.locateOnScreen(Screen_Img, grayscale=True,confidence=.9)
+
+# run code untill press s on keyboard
+while True:
+    if keyboard.is_pressed('s'):
+        break
+    else:
+        x,y,width,height=msg
+        print ("The location of the Img is:X={},Y={}".format(x,y))
+        # click the Img on the screen
+        x+=10
+        y+=10
+        pyautogui.click(x,y,button='left')
